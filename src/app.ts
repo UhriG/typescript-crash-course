82a8041d81d4c0df37f7521ac971765b24890fe0
Cristian Uhrig <cristian.uhrig@wcanvas.com>
2023-03-25 13:26:06 -0300

    Feature: applying tuples in the project

--- src/app.ts
+++ src/app.ts
@@ -41,65 +41,14 @@ const list = new ListTemplate(ul);
 form.addEventListener('submit', (e: Event) => {
 	e.preventDefault();
 
+	let values: [string, string, number] = [tofrom.value, details.value, amount.valueAsNumber];
+
 	let doc: HasFormatter;
 	if (type.value === 'invoice') {
-		doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+		doc = new Invoice(...values);
 	} else {
-		doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+		doc = new Payment(...values);
 	}
 
 	list.render(doc, type.value, 'end');
 });
-
-// Generics
-
-const addUID = <T extends { name: string }>(obj: T) => {
-	let uid = Math.floor(Math.random() * 100);
-	return { ...obj, uid };
-};
-
-let docOne = addUID({ name: 'yoshi', age: 40 });
-
-console.log(docOne.name);
-
-// Enums
-enum ResourceType {
-	BOOK,
-	AUTHOR,
-	FILM,
-	DIRECTOR,
-	PERSON,
-}
-
-// with interfaces
-interface Resource<T> {
-	uid: number;
-	resourceType: ResourceType.PERSON;
-	data: T;
-}
-
-const docThree: Resource<object> = {
-	uid: 1,
-	resourceType: ResourceType.PERSON,
-	data: { name: 'shaun' },
-};
-
-const docFour: Resource<string[]> = {
-	uid: 2,
-	resourceType: ResourceType.PERSON,
-	data: ['bread', 'milk', 'toilet roll'],
-};
-
-// Tuples
-
-let arr = ['ryu', 25, true];
-arr[0] = false;
-arr[1] = 'yoshi';
-arr = [30, false, 'yoshi'];
-
-let tup: [string, number, boolean] = ['ryu', 25, true];
-tup[0] = 'ken';
-tup[1] = 30;
-
-let student: [string, number];
-student = ['chun-li', 223423];
